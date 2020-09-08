f26898a77a24ec084073ce8eb3b79302f804b85d
MYR <mercer108@gmail.com>
2020-09-08 21:01:07 +0300

    Add signalR dependency Replace unnecessary files to Optional

--- Client/client/src/App.tsx
+++ Client/client/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import signalR from "@microsoft/signalr";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const hubConnection = new signalR.HubConnectionBuilder()
+		.withUrl("/chat")
+		.build();
+
+	hubConnection.on("Send", function (data) {
+		let elem = document.createElement("p");
+		elem.appendChild(document.createTextNode(data));
+		let firstElem = document.getElementById("chatroom").firstChild;
+		document.getElementById("chatroom").insertBefore(elem, firstElem);
+	});
+	document.getElementById("sendBtn").addEventListener("click", function (e) {
+		let message = document.getElementById("message").value;
+		hubConnection.invoke("Send", message);
+	});
+
+	hubConnection.start();
+	return (
+		<div className="App">
+			<div id="inputForm">
+				<input type="text" id="message" />
+				<input type="button" id="sendBtn" value="Отправить" />
+			</div>
+			<div id="chatroom"></div>
+		</div>
+	);
 }
 
 export default App;

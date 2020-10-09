c316ddbb65e2b9bb097e73356860e1c2cff8dbc3
MYR <mercer108@gmail.com>
2020-10-09 14:03:00 +0300

    Wrap fetch in trycatch

--- Client/client/src/api/api.ts
+++ Client/client/src/api/api.ts
@@ -6,7 +6,12 @@ const API_ENDPOINT: string = "http://192.168.0.106:5000/api";
 
 class APIService {
 	public static CreateRoom = async () => {
-		let response = await fetch(API_ENDPOINT + "/room/create");
+		let response: Response;
+		try {
+			response = await fetch(API_ENDPOINT + "/room/create");
+		} catch (error) {
+			throw new RoomError(ErrorCodes.ServerIsUnavailable);
+		}
 		if (response.ok) {
 			let roomCode = await response.text();
 			return roomCode;
@@ -16,21 +21,30 @@ class APIService {
 	};
 
 	public static GetRoomState = async (code: string) => {
-		let response = await fetch(API_ENDPOINT + "/room?" + new URLSearchParams({
-			code: code
-		}));
+		let response: Response;
+		try {
+			response = await fetch(
+				API_ENDPOINT +
+					"/room?" +
+					new URLSearchParams({
+						code: code,
+					})
+			);
+		} catch (error) {
+			throw new RoomError(ErrorCodes.ServerIsUnavailable);
+		}
 		if (response.ok) {
 			return true;
 		} else {
-			if (response.status === HttpStatusCodes.NOT_FOUND)
-			{
-				let errorCode:number = await response.json();
-				switch(errorCode)
-				{
-					case 0: { 	// RoomIsFull
+			if (response.status === HttpStatusCodes.NOT_FOUND) {
+				let errorCode: number = await response.json();
+				switch (errorCode) {
+					case 0: {
+						// RoomIsFull
 						throw new RoomError(ErrorCodes.RoomIsFull);
 					}
-					case 1: {	// RoomNotFound,
+					case 1: {
+						// RoomNotFound,
 						throw new RoomError(ErrorCodes.RoomNotFound);
 					}
 				}

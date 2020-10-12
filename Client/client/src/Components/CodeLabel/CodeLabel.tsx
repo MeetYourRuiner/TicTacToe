81211ad86edcee387a5c2ef04530163483d073e6
MYR <mercer108@gmail.com>
2020-10-12 17:51:46 +0300

    Add copy icon Implement click-to-copy on code label

--- Client/client/src/Components/CodeLabel/CodeLabel.tsx
+++ Client/client/src/Components/CodeLabel/CodeLabel.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
 import "./CodeLabel.css";
+import copyIcon from "../../img/copy.png";
 
 interface ICodeLabelProps {
 	value: string;
 }
 
 function CodeLabel(props: ICodeLabelProps) {
+
+	const handleClick = () => {
+		if (navigator.clipboard) {
+			navigator.clipboard.writeText(props.value);
+		}
+	}
+
 	return (
-		<div className="code-label-wrapper">
+		<div  onClick={() => handleClick()} className="code-label-wrapper">
+			<img className="code-label-icon" src={copyIcon}/>
 			<div className="code-label-text">{props.value}</div>
 		</div>
 	);
